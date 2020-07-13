066c64d0cd2859b0d24d15d39b5fb86e15f50982
Jay Fenwick <fenwickjb@appstate.edu>
2020-07-13 15:06:52 -0400

    improved comments

--- src/AppStateWeatherData.java
+++ src/AppStateWeatherData.java
@@ -1,35 +1,60 @@
 /*
   AppStateWeatherData.java
 
+  This is a "boundary object" for our weather software system.
+  
+  Our boundary object connects the rest of our system with the
+  device API. This way, the other parts of our system do not
+  need to fret over device particulars. We offer a nice
+  and easy to use set of methods.
+
   Based on Head First Design Patterns
   @author Jay Fenwick
   @version 2018
 */
 
 public class AppStateWeatherData implements WeatherDataInterface {
+
+    // Reference to actual device via WeatherData API object
     WeatherData weatherStation;
 
+    // Our various ways of displaying weather data
     CurrentConditionsDisplay condDisplay;
     StatisticsDisplay statDisplay;
     ForecastDisplay forecastDisplay;
     
 
     public AppStateWeatherData(WeatherData station) {
-	// Setup two-way connection
+
+	// Setup two-way connection between us and device:
+	//  we know about them:
 	weatherStation = station;
+	//  tell them about us:
 	station.connect(this);
 
+	// Create our displays
 	condDisplay = new CurrentConditionsDisplay();
 	statDisplay = new StatisticsDisplay();
 	forecastDisplay = new ForecastDisplay();
     }
+
     
-    /* WeatherDataInterface API */
+    /* WeatherDataInterface API requires this method. Since they know about us,
+       when weather conditions change they can alert us. But they have to know
+       how to contact us; so we implemented the WeatherDataInterface which 
+       requires us to implement this method and then they know that we have
+       this method available!! (Dang, interfaces are kinda cool like that!) */
+
     public void measurementsChanged() {
+
+	/* Weather station device has picked up changes in weather and
+	   called this method. Now, we need to pull current values 
+	   from them. */
 	float t = weatherStation.getCurrTemp();
 	float h = weatherStation.getCurrHumid();
 	float p = weatherStation.getCurrPress();
 
+	// With new weather condition data, we need to update our displays.
 	condDisplay.update(t, h, p);
 	statDisplay.update(t, h, p);
 	forecastDisplay.update(t, h, p);
